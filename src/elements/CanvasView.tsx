4c0935cad5f048e0847052030a8f87eab76afbd5
gct256 <gct256+github@gmail.com>
2019-06-11 22:08:04 +0900

    Enhancement CanvasView.
    - add renderMode props.
    - use double buffer rendering.

--- src/elements/CanvasView.tsx
+++ src/elements/CanvasView.tsx
@@ -2,6 +2,32 @@ import * as React from 'react';
 
 import { BasicProps } from '../utils/commonProps';
 
+type RenderMode = 'default' | 'clearly' | 'fine';
+
+function getPixelRatio(): number {
+  const ratio = window.devicePixelRatio;
+
+  if (typeof ratio !== 'number' || !Number.isFinite(ratio) || ratio < 1) {
+    return 1;
+  }
+
+  return Math.ceil(ratio);
+}
+
+function getScales(renderMode?: RenderMode): [number, number] {
+  const ratio = getPixelRatio();
+
+  switch (renderMode) {
+    case 'fine':
+      return [ratio, ratio];
+    case 'clearly':
+      return [ratio, 1];
+
+    default:
+      return [1, 1];
+  }
+}
+
 interface CanvasViewProps extends BasicProps {
   /** Width of canvas element. */
   width: number;
@@ -9,6 +35,8 @@ interface CanvasViewProps extends BasicProps {
   height: number;
   /** Serial value to control redraw. */
   serial: number | string;
+  /** Rendering mode. */
+  renderMode?: RenderMode;
 
   /**
    * Event handler on redraw.
@@ -17,25 +45,29 @@ interface CanvasViewProps extends BasicProps {
     context: CanvasRenderingContext2D,
     width: number,
     height: number,
+    scale: number,
     serial: number | string,
   ) => void;
 }
 
 /** Controlable HTML Canvas element. */
 export class CanvasView extends React.Component<CanvasViewProps> {
-  private ref: React.RefObject<HTMLCanvasElement> = React.createRef();
+  private ref1: React.RefObject<HTMLCanvasElement> = React.createRef();
+
+  private ref2: React.RefObject<HTMLCanvasElement> = React.createRef();
 
   public componentDidMount() {
     this.renderToCanvas(this.props);
   }
 
   public shouldComponentUpdate(nextProps: CanvasViewProps): boolean {
-    const { className, width, height, serial } = this.props;
+    const { className, width, height, renderMode, serial } = this.props;
 
     if (
       className !== nextProps.className ||
       width !== nextProps.width ||
-      height !== nextProps.height
+      height !== nextProps.height ||
+      renderMode !== nextProps.renderMode
     ) {
       return true;
     }
@@ -52,27 +84,59 @@ export class CanvasView extends React.Component<CanvasViewProps> {
   }
 
   private renderToCanvas(props: CanvasViewProps) {
-    if (this.ref.current === null) return;
+    if (this.ref1.current === null || this.ref2.current === null) return;
 
-    const context = this.ref.current.getContext('2d');
+    const front = this.ref1.current.getContext('2d');
+    const back = this.ref2.current.getContext('2d');
 
-    if (context === null) return;
+    if (front === null || back === null) return;
 
-    const { width, height, serial, onRedraw } = props;
+    const { width, height, serial, renderMode, onRedraw } = props;
+    const [scale1, scale2] = getScales(renderMode);
 
-    onRedraw(context, width, height, serial);
+    back.setTransform(1, 0, 0, 1, 0, 0);
+    back.scale(scale2, scale2);
+    onRedraw(back, width, height, scale2, serial);
+
+    front.imageSmoothingEnabled = renderMode !== 'clearly';
+    front.clearRect(0, 0, width * scale1, height * scale1);
+    front.drawImage(
+      back.canvas,
+      0,
+      0,
+      width * scale2,
+      height * scale2,
+      0,
+      0,
+      width * scale1,
+      height * scale1,
+    );
   }
 
   public render() {
-    const { className, width, height } = this.props;
+    const { className, width, height, renderMode } = this.props;
+    const [scale1, scale2] = getScales(renderMode);
+    const style = {
+      width: `${width}px`,
+      height: `${height}px`,
+    };
 
     return (
-      <canvas
-        className={className}
-        ref={this.ref}
-        width={width}
-        height={height}
-      />
+      <>
+        <canvas
+          className={className}
+          ref={this.ref1}
+          width={width * scale1}
+          height={height * scale1}
+          style={style}
+        />
+        <canvas
+          className="hidden"
+          ref={this.ref2}
+          width={width * scale2}
+          height={height * scale2}
+        />
+      </>
     );
   }
 }
--- stories/elements/canvasView.stories.tsx
+++ stories/elements/canvasView.stories.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number, select } from '@storybook/addon-knobs';
 
 import { CanvasView } from '../../src';
+
+const redraw = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  _scale: number,
+  serial: number | string,
+) => {
+  // eslint-disable-next-line no-param-reassign
+  context.font = '24px serif';
+  context.clearRect(0, 0, width, height);
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(width, height);
+  context.stroke();
+  context.beginPath();
+  context.arc(125, 125, 50, 0, Math.PI * 2, true);
+  context.stroke();
+  context.fillText(`serial: ${serial}`, 50, 50);
+};
 
 storiesOf('elements / CanvasView', module)
   .addDecorator(withKnobs)
@@ -22,14 +42,26 @@ storiesOf('elements / CanvasView', module)
           range: true,
         })}
         serial={number('serial', 0)}
-        onRedraw={(context, width, height, serial) => {
-          context.clearRect(0, 0, width, height);
-          context.beginPath();
-          context.moveTo(0, 0);
-          context.lineTo(width, height);
-          context.stroke();
-          context.fillText(`serial: ${serial}`, 20, 20);
-        }}
+        onRedraw={redraw}
+      />
+    </>
+  ))
+  .add('render mode', () => (
+    <>
+      <CanvasView
+        width={250}
+        height={250}
+        renderMode={select(
+          'render mode',
+          {
+            default: 'default',
+            clearly: 'clearly',
+            fine: 'fine',
+          },
+          'default',
+        )}
+        serial={0}
+        onRedraw={redraw}
       />
     </>
   ));
