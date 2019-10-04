3f461352d5805c5713c44f343413d43fcc83f7b2
gct256 <gct256+github@gmail.com>
2019-10-04 17:49:12 +0900

    Add valign props to FormRow.

--- src/forms/FormRow.tsx
+++ src/forms/FormRow.tsx
@@ -5,17 +5,23 @@ import { BasicProps } from '../utils/commonProps';
 
 type FormRowProps = BasicProps & {
   visible?: boolean;
+  valign?: 'top' | 'bottom' | 'middle';
 };
 
 /** Form's row element. */
 export const FormRow: React.FC<FormRowProps> = ({
   className,
   visible,
+  valign,
   children,
 }: React.PropsWithChildren<FormRowProps>) =>
   visible ? (
     <div
-      className={classnames(className, 'p-1 flex justify-start items-start')}
+      className={classnames(className, 'p-1 flex justify-start', {
+        'items-start': valign !== 'middle' && valign !== 'bottom',
+        'items-center': valign === 'middle',
+        'items-end': valign === 'bottom',
+      })}
     >
       {children}
     </div>
