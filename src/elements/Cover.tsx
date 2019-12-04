1289d3672edb680dddcbdc537c154da86a4064ed
gct256 <gct256+github@gmail.com>
2019-12-04 19:04:52 +0900

    Added modal props to Cover element.

--- src/elements/Cover.tsx
+++ src/elements/Cover.tsx
@@ -1,27 +1,63 @@
 import * as React from 'react';
 import classnames from 'classnames';
+import FocusTrap from 'focus-trap-react';
 
 import { BasicProps } from '../utils/commonProps';
 
 type CoverProps = BasicProps & {
+  /** ClassName for content. */
   contentClassName?: string;
+  /** If true, cover appeared. */
   visible?: boolean;
+  /** If true, trapping focus. */
+  modal?: boolean;
 };
+
+const CoverBody: React.FC<CoverProps> = ({
+  className,
+  contentClassName,
+  children,
+}: React.PropsWithChildren<CoverProps>) => (
+  <>
+    <button type="button" className="absolute w-0 h-0 overflow-hidden">
+      cover
+    </button>
+    <div
+      className={classnames('fixed inset-0 z-99999 select-none', className)}
+    />
+    <div className={classnames('fixed z-99999 select-none', contentClassName)}>
+      {children}
+    </div>
+  </>
+);
 
 export const Cover: React.FC<CoverProps> = ({
   className,
   contentClassName,
   visible,
+  modal,
   children,
-}: React.PropsWithChildren<CoverProps>) =>
-  visible ? (
-    <>
-      <div className={classnames('fixed inset-0 z-99999', className)} />
-      <div className={classnames('fixed z-99999', contentClassName)}>
-        {children}
+}: React.PropsWithChildren<CoverProps>) => {
+  if (!visible) return null;
+
+  return modal ? (
+    <FocusTrap
+      focusTrapOptions={{
+        escapeDeactivates: false,
+      }}
+    >
+      <div>
+        <CoverBody className={className} contentClassName={contentClassName}>
+          {children}
+        </CoverBody>
       </div>
-    </>
-  ) : null;
+    </FocusTrap>
+  ) : (
+    <CoverBody className={className} contentClassName={contentClassName}>
+      {children}
+    </CoverBody>
+  );
+};
 
 Cover.displayName = 'Cover';
 
--- stories/elements/cover.stories.tsx
+++ stories/elements/cover.stories.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import useState from 'storybook-addon-state';
 
 import { ExampleStage } from '../utils/ExampleStage';
-import { Cover, Icon } from '../../src';
+import { Cover, Icon, Button } from '../../src';
 import { ExampleLorem } from '../utils/ExampleLorem';
 
 export default {
@@ -49,3 +50,38 @@ export const setContentClassName = () => (
     </Cover>
   </ExampleStage>
 );
+
+export const notModal = () => {
+  const [visible, setVisible] = useState('visible', false);
+  const handleClick = React.useCallback(() => {
+    setVisible(!visible);
+  }, [visible]);
+
+  return (
+    <ExampleStage>
+      <Button onClick={handleClick}>Toggle Cover</Button>
+      <Cover visible={visible}>
+        <p>Cover message</p>
+        <Icon icon={faSpinner} spin className="mt-2" />
+      </Cover>
+    </ExampleStage>
+  );
+};
+
+export const modal = () => {
+  const [visible, setVisible] = useState('visible', false);
+  const handleClick = React.useCallback(() => {
+    setVisible(true);
+    setTimeout(() => setVisible(false), 1000);
+  }, [visible]);
+
+  return (
+    <ExampleStage>
+      <Button onClick={handleClick}>Show Cover</Button>
+      <Cover visible={visible} modal>
+        <p>Wait 1sec.</p>
+        <Icon icon={faSpinner} spin className="mt-2" />
+      </Cover>
+    </ExampleStage>
+  );
+};
