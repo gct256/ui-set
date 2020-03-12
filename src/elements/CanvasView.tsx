dd4f8f4355633851476ecc7a05a26e1216e3ffab
gct256 <gct256+github@gmail.com>
2020-03-12 21:56:27 +0900

    Rewrote CanvasView to functional component.

--- src/elements/CanvasView.tsx
+++ src/elements/CanvasView.tsx
@@ -51,74 +51,54 @@ type CanvasViewProps = BasicProps & {
   ) => void;
 };
 
+const renderToCanvas = (
+  props: CanvasViewProps,
+  ref1: React.RefObject<HTMLCanvasElement>,
+  ref2: React.RefObject<HTMLCanvasElement>,
+): void => {
+  if (ref1.current === null || ref2.current === null) return;
+
+  const front = ref1.current.getContext('2d');
+  const back = ref2.current.getContext('2d');
+
+  if (front === null || back === null) return;
+
+  const { width, height, serial, renderMode, onRedraw } = props;
+
+  if (width === 0 || height === 0) return;
+
+  const [scale1, scale2] = getScales(renderMode);
+
+  back.setTransform(1, 0, 0, 1, 0, 0);
+  back.scale(scale2, scale2);
+  onRedraw(back, width, height, scale2, serial);
+
+  front.imageSmoothingEnabled = renderMode !== 'clearly';
+  front.clearRect(0, 0, width * scale1, height * scale1);
+  front.drawImage(
+    back.canvas,
+    0,
+    0,
+    width * scale2,
+    height * scale2,
+    0,
+    0,
+    width * scale1,
+    height * scale1,
+  );
+};
+
 /** Controlable HTML Canvas element. */
-export class CanvasView extends React.Component<CanvasViewProps> {
-  private ref1: React.RefObject<HTMLCanvasElement> = React.createRef();
+export const CanvasView: React.FC<CanvasViewProps> = React.memo(
+  (props: CanvasViewProps) => {
+    const ref1 = React.useRef<HTMLCanvasElement>(null);
+    const ref2 = React.useRef<HTMLCanvasElement>(null);
 
-  private ref2: React.RefObject<HTMLCanvasElement> = React.createRef();
+    React.useEffect(() => {
+      renderToCanvas(props, ref1, ref2);
+    }, [props]);
 
-  public componentDidMount(): void {
-    this.renderToCanvas(this.props);
-  }
-
-  public shouldComponentUpdate(nextProps: CanvasViewProps): boolean {
-    const { className, width, height, renderMode, serial } = this.props;
-
-    if (
-      className !== nextProps.className ||
-      width !== nextProps.width ||
-      height !== nextProps.height ||
-      renderMode !== nextProps.renderMode
-    ) {
-      return true;
-    }
-
-    if (serial !== nextProps.serial) {
-      this.renderToCanvas(nextProps);
-    }
-
-    return false;
-  }
-
-  public componentDidUpdate(): void {
-    this.renderToCanvas(this.props);
-  }
-
-  private renderToCanvas(props: CanvasViewProps): void {
-    if (this.ref1.current === null || this.ref2.current === null) return;
-
-    const front = this.ref1.current.getContext('2d');
-    const back = this.ref2.current.getContext('2d');
-
-    if (front === null || back === null) return;
-
-    const { width, height, serial, renderMode, onRedraw } = props;
-
-    if (width === 0 || height === 0) return;
-
-    const [scale1, scale2] = getScales(renderMode);
-
-    back.setTransform(1, 0, 0, 1, 0, 0);
-    back.scale(scale2, scale2);
-    onRedraw(back, width, height, scale2, serial);
-
-    front.imageSmoothingEnabled = renderMode !== 'clearly';
-    front.clearRect(0, 0, width * scale1, height * scale1);
-    front.drawImage(
-      back.canvas,
-      0,
-      0,
-      width * scale2,
-      height * scale2,
-      0,
-      0,
-      width * scale1,
-      height * scale1,
-    );
-  }
-
-  public render(): React.ReactNode {
-    const { className, width, height, renderMode } = this.props;
+    const { className, width, height, renderMode } = props;
     const [scale1, scale2] = getScales(renderMode);
     const style = {
       width: `${width}px`,
@@ -129,18 +109,24 @@ export class CanvasView extends React.Component<CanvasViewProps> {
       <>
         <canvas
           className={className}
-          ref={this.ref1}
+          ref={ref1}
           width={width * scale1}
           height={height * scale1}
           style={style}
         />
         <canvas
           className="hidden"
-          ref={this.ref2}
+          ref={ref2}
           width={width * scale2}
           height={height * scale2}
         />
       </>
     );
-  }
-}
+  },
+  (prevProps: CanvasViewProps, nextProps: CanvasViewProps) =>
+    prevProps.className === nextProps.className &&
+    prevProps.width === nextProps.width &&
+    prevProps.height === nextProps.height &&
+    prevProps.renderMode === nextProps.renderMode &&
+    prevProps.serial === nextProps.serial,
+);
--- stories/elements/canvasView.stories.tsx
+++ stories/elements/canvasView.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import * as React from 'react';
 import { withKnobs, number, select } from '@storybook/addon-knobs';
 
@@ -13,20 +14,30 @@ const redraw = (
   context: CanvasRenderingContext2D,
   width: number,
   height: number,
-  _scale: number,
+  scale: number,
   serial: number | string,
 ) => {
-  // eslint-disable-next-line no-param-reassign
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
   context.font = '24px serif';
+  context.fillStyle = `rgb(${r},${g},${b})`;
+  context.strokeStyle = '#000';
+
   context.clearRect(0, 0, width, height);
   context.beginPath();
   context.moveTo(0, 0);
   context.lineTo(width, height);
   context.stroke();
+
   context.beginPath();
   context.arc(125, 125, 50, 0, Math.PI * 2, true);
+  context.fill();
   context.stroke();
-  context.fillText(`serial: ${serial}`, 50, 50);
+
+  context.fillStyle = '#000';
+  context.fillText(`serial: ${serial}, scale: ${scale}`, 50, 50);
 };
 
 export const standard = () => (
